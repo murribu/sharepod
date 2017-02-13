e2a4feaf568680ba7c7111b560c8fb5e6e17fc56
EC2 Default User <ec2-user@ip-172-31-42-63.ec2.internal>
2017-02-13 15:54:53 +0000

    Add more icons to the manifest

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -39,20 +39,30 @@ class HomeController extends Controller
     public function getManifest(){
         $manifest = [
             'manifest_version'  => 1,
-            'name'              => env('APP_NAME')."1",
+            'name'              => env('APP_NAME'),
             'version'           => '0.1',
-            'short_name'        => env('APP_SHORT_NAME')."1",
+            'short_name'        => env('APP_SHORT_NAME'),
             
-            'start_url'         => '/shows',
+            'start_url'         => '/',
             'display'           => 'standalone',
-            'background_color'  => '#000',
+            'background_color'  => '#f5f8fa',
             'description'       => env('APP_DESCRIPTION'),
             'icons' => [
                     [
                         'src'       => 'img/logo.png',
                         'sizes'     => '48x48',
                         'type'      => 'image/png'
-                    ]
+                    ],
+                    [
+                        'src'       => 'img/logo_128x128.png',
+                        'sizes'     => '128x128',
+                        'type'      => 'image/png'
+                    ],
+                    [
+                        'src'       => 'img/logo_256x256.png',
+                        'sizes'     => '256x256',
+                        'type'      => 'image/png'
+                    ],
                 ],
             'related_applications' => [
                     [
