7be5cde424b3165a1a2d325a418d48a768c46314
Cory <murribu@gmail.com>
2017-02-13 15:59:37 +0000

    remove debug code

--- app/Console/Commands/RefreshFeed.php
+++ app/Console/Commands/RefreshFeed.php
@@ -9,7 +9,6 @@ class RefreshFeed extends Command
     
     public function handle() {
         if ($this->option('show_id')){
-            dd($this->option('show_id'));
             $show = Show::find($this->option('show_id'));
             $ret = $show->parseFeed();
         }else{
