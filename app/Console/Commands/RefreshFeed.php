7fee240dc41ce5222b3115fff52128554a0190d2
Cory <murribu@gmail.com>
2017-02-13 15:58:03 +0000

    bug fix in refreshfeed

--- app/Console/Commands/RefreshFeed.php
+++ app/Console/Commands/RefreshFeed.php
@@ -5,10 +5,11 @@ use Illuminate\Console\Command;
 use App\Show;
 class RefreshFeed extends Command
 {
-    protected $signature = 'refreshfeed {--show_id=false}';
+    protected $signature = 'refreshfeed {--show_id=0}';
     
     public function handle() {
         if ($this->option('show_id')){
+            dd($this->option('show_id'));
             $show = Show::find($this->option('show_id'));
             $ret = $show->parseFeed();
         }else{
