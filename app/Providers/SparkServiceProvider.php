f1b45e58a7ae48d3dfea2656da6bbada58317e53
Cory Martin <murribu@gmail.com>
2017-02-28 22:46:01 +0000

    php bug fix in calculating a plan's storage

--- app/Providers/SparkServiceProvider.php
+++ app/Providers/SparkServiceProvider.php
@@ -70,7 +70,7 @@ class SparkServiceProvider extends ServiceProvider
             ->features([
                 'Send up to '.env('PLAN_BASIC_RECOMMENDATION_COUNT').' recommendations per 24 hours', 
                 'Create up to '.env('PLAN_BASIC_PLAYLIST_COUNT').' playlists',
-                'Archive episodes (up to '.(intval(env('PLAN_BASIC_STORAGE_LIMIT'))/1000000).' MB)',
+                'Archive episodes (up to '.(intval(env('PLAN_BASIC_STORAGE_LIMIT'))/pow(2,20)).' MB)',
             ]);
             
         Spark::plan('Premium', 'premium-1')
@@ -78,7 +78,7 @@ class SparkServiceProvider extends ServiceProvider
             ->features([
                 'Send unlimited recommendations', 
                 'Create unlimited playlists',
-                'Archive episodes (up to '.(intval(env('PLAN_PREMIUM_STORAGE_LIMIT'))/1000000000).' GB)',
+                'Archive episodes (up to '.(intval(env('PLAN_PREMIUM_STORAGE_LIMIT'))/pow(2,30)).' GB)',
                 'Archive shows (Coming soon)',
                 'Secure playlists with a username and password (Coming soon)'
             ]);
