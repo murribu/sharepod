ad8869e097f7d650377bcf6f6bb3ddf4c0e8bd89
Cory Martin <murribu@gmail.com>
2017-03-03 15:08:19 +0000

    Add the temp mysql backup storage location to clean_out_tmp

--- app/Console/Commands/CleanOutTmp.php
+++ app/Console/Commands/CleanOutTmp.php
@@ -23,11 +23,15 @@ class CleanOutTmp extends Command{
 
         if(!flock($fh, LOCK_EX)) // another process is running
             return [];
-
-        $files = glob('/tmp/*.mp3'); // get all mp3 files
-        foreach($files as $file){ // iterate files
-            if(is_file($file))
-                unlink($file); // delete file
+        
+        
+        $tmp_repos = ['/tmp/*.mp3', storage_path('app/*.gz')];
+        foreach($tmp_repos as $path){
+            $files = glob($path); // get all files
+            foreach($files as $file){ // iterate files
+                if(is_file($file))
+                    unlink($file); // delete file
+            }
         }
         
         flock($fh, LOCK_UN);
