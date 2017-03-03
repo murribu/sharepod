4bded7bc50aa044cfa75fdbb991b38113916082c
Cory Martin <murribu@gmail.com>
2017-03-03 14:48:47 +0000

    Catch the loadHTML() error when processing mal-formed html in an episode's description

--- app/Episode.php
+++ app/Episode.php
@@ -312,42 +312,45 @@ class Episode extends Model {
             'body' => [],
         ];
         $dom = new \DOMDocument();
-        $dom->loadHTML(mb_convert_encoding($input , 'HTML-ENTITIES', 'UTF-8'));
-        $xpath = new \DOMXPath($dom);
-        $nodes = $xpath->query('//*');
-        foreach ($nodes as $node) {
-            $tagName = $node->tagName;
-            $nodeName = $node->nodeName;
-            $nodeValue = $node->nodeValue;
-            if (isset($tags[$tagName])){
-                foreach ($node->attributes as $attr){
-                    if (isset($tags[$tagName]['allow_attr'][$attr->name])){
-                        $protocol_is_ok = false;
-                        if (isset($tags[$tagName]['allow_attr'][$attr->name]['allow_to_begin_with'])){
-                            foreach($tags[$tagName]['allow_attr'][$attr->name]['allow_to_begin_with'] as $proto){
-                                if ($proto == substr($attr->value, 0, strlen($proto))){
-                                    $protocol_is_ok = true;
+        if (@$dom->loadHTML(mb_convert_encoding($input , 'HTML-ENTITIES', 'UTF-8'))){
+            $xpath = new \DOMXPath($dom);
+            $nodes = $xpath->query('//*');
+            foreach ($nodes as $node) {
+                $tagName = $node->tagName;
+                $nodeName = $node->nodeName;
+                $nodeValue = $node->nodeValue;
+                if (isset($tags[$tagName])){
+                    foreach ($node->attributes as $attr){
+                        if (isset($tags[$tagName]['allow_attr'][$attr->name])){
+                            $protocol_is_ok = false;
+                            if (isset($tags[$tagName]['allow_attr'][$attr->name]['allow_to_begin_with'])){
+                                foreach($tags[$tagName]['allow_attr'][$attr->name]['allow_to_begin_with'] as $proto){
+                                    if ($proto == substr($attr->value, 0, strlen($proto))){
+                                        $protocol_is_ok = true;
+                                    }
+                                }
+                                if (!$protocol_is_ok){
+                                    $node->removeAttribute($attr->name);
                                 }
                             }
-                            if (!$protocol_is_ok){
-                                $node->removeAttribute($attr->name);
+                            foreach($tags[$tagName]['insert_attr'] as $attr=>$val){
+                                $node->setAttribute($attr, $val);
                             }
+                        }else{
+                            $node->removeAttribute($attr->name);
                         }
-                        foreach($tags[$tagName]['insert_attr'] as $attr=>$val){
-                            $node->setAttribute($attr, $val);
-                        }
-                    }else{
-                        $node->removeAttribute($attr->name);
                     }
+                }else{
+                    $node->parentNode->removeChild($node);
                 }
-            }else{
-                $node->parentNode->removeChild($node);
             }
-        }
-        $output = $dom->saveHTML($dom->getElementsByTagName('html')->item(0)->getElementsByTagName('body')->item(0)->getElementsByTagName('p')->item(0));
-        
-        $output = substr($output, 3, count($output) - 5);
+            $output = $dom->saveHTML($dom->getElementsByTagName('html')->item(0)->getElementsByTagName('body')->item(0)->getElementsByTagName('p')->item(0));
+            
+            $output = substr($output, 3, count($output) - 5);
 
-        return $output;
+            return $output;
+        }else{
+            return strip_tags($this->description, '<p></p>');
+        }
     }
 }
